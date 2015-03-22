4d9cf1bae7d2f574db8a46b61df6cdf0e0bbaf1f
james <dickfala@gmail.com>
2015-03-22 18:25:20 +0800

    Initial Commit

---
+++ PopTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PopTest
+//
+//  Created by Chang YuanYu on 2015/3/22.
+//  Copyright (c) 2015年 yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PopTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PopTest
+//
+//  Created by Chang YuanYu on 2015/3/22.
+//  Copyright (c) 2015年 yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
